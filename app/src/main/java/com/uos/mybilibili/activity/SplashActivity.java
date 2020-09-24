5c37fc8d628e59b97683ecadfa4de2dc1850c084
xuxc <xxch7219@163.com>
2020-09-24 18:26:43 +0800

    仿制Bilibili初始化工程

---
+++ app/src/main/java/com/uos/mybilibili/activity/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.uos.mybilibili.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.uos.mybilibili.R;
+
+/**
+ * Author: XXC
+ * Date: 2020/9/24 18:04
+ * Desc: 闪屏页(广告页)
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
